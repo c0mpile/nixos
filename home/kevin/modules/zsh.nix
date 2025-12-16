096273b12fe1d96b55000944fb6f202045342363
c0mpile <c0mpile@disroot.org>
2025-12-15 21:52:30 -0500

    refactor: use lib.mkMerge for zsh initContent and add instant prompt

--- home/kevin/modules/zsh.nix
+++ home/kevin/modules/zsh.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   programs.zsh = {
@@ -35,40 +35,35 @@
       ];
     };
 
-    initContent = ''
-      # Source Powerlevel10k config
-      if [[ -r "${./zsh/p10k.zsh}" ]]; then
-        source "${./zsh/p10k.zsh}"
-        
-        if [[ "''${LC_ALL:-''${LC_CTYPE:-$LANG}}" =~ "UTF-8" ]] && (( ''${terminfo[colors]:-0} >= 256 )); then
-          # High capabilities: Use prompt loop with NO icons
-          typeset -g POWERLEVEL9K_VISUAL_IDENTIFIER_EXPANSION=""
-          typeset -g POWERLEVEL9K_VCS_BRANCH_ICON=""
-          typeset -g POWERLEVEL9K_LOCK_ICON=""
-        else
-          # Low capabilities: ASCII mode with ANSI colors
-          typeset -g POWERLEVEL9K_MODE=ascii
-          typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_VIINS_CONTENT_EXPANSION=">"
-          typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_VIINS_CONTENT_EXPANSION=">"
-          typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_VICMD_CONTENT_EXPANSION="<"
-          typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_VICMD_CONTENT_EXPANSION="<"
-          typeset -g POWERLEVEL9K_VISUAL_IDENTIFIER_EXPANSION=""
-          
-          # Colors (ANSI)
-          typeset -g POWERLEVEL9K_DIR_FOREGROUND=4
-          typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=4
-          typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=4
-          typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=2
-          typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=1
+    initContent = lib.mkMerge [
+      (lib.mkBefore ''
+        # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+        # Initialization code that may require console input (password prompts, [y/n]
+        # confirmations, etc.) must go above this block; everything else may go below.
+        if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+          source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
         fi
-      fi
+      '')
 
-      # Keybindings for history search
-      bindkey '^[[A' history-substring-search-up
-      bindkey '^[[B' history-substring-search-down
+      ''
+        # Source Powerlevel10k config
+        if [[ "''${LC_ALL:-''${LC_CTYPE:-$LANG}}" =~ "UTF-8" ]] && (( ''${terminfo[colors]:-0} >= 256 )); then
+           if [[ -r "${./zsh/p10k.zsh}" ]]; then
+             source "${./zsh/p10k.zsh}"
+           fi
+        else
+           if [[ -r "${./zsh/p10k-ascii.zsh}" ]]; then
+             source "${./zsh/p10k-ascii.zsh}"
+           fi
+        fi
 
-      # Sudo plugin keybinding (Alt-s)
-      bindkey '^[s' sudo-command-line
-    '';
+        # Keybindings for history search
+        bindkey '^[[A' history-substring-search-up
+        bindkey '^[[B' history-substring-search-down
+
+        # Sudo plugin keybinding (Alt-s)
+        bindkey '^[s' sudo-command-line
+      ''
+    ];
   };
 }
